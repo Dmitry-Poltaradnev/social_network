fa147241e060d7c21366e1648692fda71be36d0d
Dmitry-Poltaradnev <poltaradnev@gmail.com>
2024-08-05 18:20:21 +0400

    feat: test logic for App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+
+    const [state, setState] = useState(['1', '323', '1', 20, '110'])
+
+    const deleteItem = (item1: any) => {
+        setState(state.filter(item => item !== item1))
+    }
+
+    return (
+        <div className="App">
+            {state.map(item => <ul>
+                <li>
+                    <button onClick={() => deleteItem(item)}>deleted</button>
+                    {item}</li>
+            </ul>)}
+        </div>
+    );
 }
 
 export default App;
